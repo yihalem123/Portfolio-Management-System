9d3a38f4b5ae66eff60b2f02fe6aee8aad096172
yihalem123 <70970259+yihalem123@users.noreply.github.com>
2024-03-14 14:46:39 +0300

    Update settings.py

--- portfolio_management_system/portfolio_management_system/settings.py
+++ portfolio_management_system/portfolio_management_system/settings.py
@@ -148,12 +148,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '../static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Required for all-auth
 SITE_ID = 1
