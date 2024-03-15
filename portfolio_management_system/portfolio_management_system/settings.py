a2585f70ea6c17e3de2bf1949607b30a809281e8
yihalem123 <70970259+yihalem123@users.noreply.github.com>
2024-03-15 12:52:22 +0300

    Update settings.py

--- portfolio_management_system/portfolio_management_system/settings.py
+++ portfolio_management_system/portfolio_management_system/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+   # os.path.join(BASE_DIR, 'static'),
 ]
 # Required for all-auth
 SITE_ID = 1
